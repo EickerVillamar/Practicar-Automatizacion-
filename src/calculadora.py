4247897822d527da126e774b3bdef1906c5076ca
Eicker <eicker.rv@gmail.com>
2026-07-11 16:47:53 -0500

    Tercera prueba

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,4 +1,4 @@
-def sumar(a,b) : return a + b 
+def sumar(a,b) : return a + b
 def restar(a,b) : return a - b
 def multiplicar(a,b) : return a *b 
 def dividir(a,b):
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,9 +1,29 @@
 from src.calculadora import sumar,restar, multiplicar, dividir
 
 def test_sumar_dos_numeros():
-    resultado = sumar(2,3)
+    # Preparar
+    a = 2
+    b = 3
+    # Ejecutar
+    resultado = sumar(a, b)
+    # Comprobar
     assert resultado == 5
+
+def test_sumar_valores_negativos():
+    a = -5
+    b = -6
     
+    resultado = sumar(a,b)
+    
+    assert resultado == -11
+    
+def test_sumar_con_ceros():
+    a = 10
+    b = 0
+    resultado = sumar(a,b)
+    assert resultado == 10
+
+
 def test_restar_dos_numeros():
     resultado = restar(8,6)
     assert resultado == 2
