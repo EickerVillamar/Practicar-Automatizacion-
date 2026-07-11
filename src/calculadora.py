5002c4b401582b8dc4f159da9033041921c0bf3e
Eicker <eicker.rv@gmail.com>
2026-07-11 16:31:02 -0500

    Dos funciones nuevas agregadas

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,4 +1,12 @@
 def sumar(a,b) : return a + b 
 def restar(a,b) : return a - b
+def multiplicar(a,b) : return a *b 
+def dividir(a,b):
+    if b==0:
+       return "No es posible dividir para cero"
+    return a/b
+
 
 print(sumar(5,5))
+
+print(dividir(8,1))
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,4 +1,4 @@
-from src.calculadora import sumar,restar
+from src.calculadora import sumar,restar, multiplicar, dividir
 
 def test_sumar_dos_numeros():
     resultado = sumar(2,3)
@@ -7,6 +7,14 @@ def test_sumar_dos_numeros():
 def test_restar_dos_numeros():
     resultado = restar(8,6)
     assert resultado == 2
+    
+def test_multiplicar():
+    resultado = multiplicar(5,5)
+    assert resultado == 25 
+    
+def test_dividir():
+    resultado = dividir(8,0)
+    assert resultado == "No es posible dividir para cero"
     
 
     
